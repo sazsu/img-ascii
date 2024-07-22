9f36262d471a56c13c5c4041a389c8a39394f059
sazsu <shonniy@yandex.ru>
2024-07-22 21:34:04 +0000

    feat: add func to calculate average brightness of image tile

--- main.py
+++ main.py
@@ -5,28 +5,17 @@ def convert_to_grayscale(img):
     im = Image.open(img)
 
     gray_im = ImageOps.grayscale(im)
-    print(gray_im.getpixel((0, 0)))
+    gray_im.thumbnail((400, 400))  # resize image
+    
     return gray_im
 
+def get_average_of_tile(img):
+    img = Image.open(img)
+    width, height = img.size
+    values = []
 
-# GRAYSCALE =  '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}'
-GRAYSCALE = '`^\”,:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+    for x in range(width):
+        for y in range(height):
+            values.append(img.getpixel((x, y)))
 
-
-def create_ascii(g_im):
-
-    width, height = g_im.size
-    pixels = g_im.load()
-    ans = []
-
-    for y in range(0, height, 2):
-        for x in range(0, width - 1, 2):
-            pix1 = pixels[x, y] // 3
-            pix2 = pixels[x + 1, y] // 3
-            pix3 = pixels[x, y + 1] // 3
-            pix4 = pixels[x + 1, y + 1] // 3
-            res = (pix1 + pix2 + pix3 + pix4) // 4
-            ascii_index = int((len(GRAYSCALE) - 1) * (res / 255))
-            ans.append(GRAYSCALE[ascii_index])
-        ans.append('\n')
-    return ''.join(ans)       
+    return sum(values) // len(values)
